047d7cbb79e870b5be72ae123daf76acecf641cc
SDINAHET <stephane.dinahet@gmail.com>
2024-08-13 15:47:19 +0200

    fix silencieusement my_exit.c  avec juste exit(status);

--- my_exit.c
+++ my_exit.c
@@ -11,16 +11,5 @@
  */
 void my_exit(int status)
 {
-	/* Afficher un message avant de quitter (facultatif)*/
-	if (status == EXIT_SUCCESS)
-	{
-		printf("Exiting with success.\n");
-	}
-	else
-	{
-		printf("Exiting with error status %d.\n", status);
-	}
-
-	/* Terminer le programme avec le statut spécifié */
 	exit(status);
 }
