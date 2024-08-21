bf1b1e71ab54e55c35c65efb8af4dedeb02675d6
Gromuf <luigi.beaumois@gmail.com>
2024-08-21 10:55:29 +0200

    betty is_space.c

--- is_space.c
+++ is_space.c
@@ -7,13 +7,10 @@
  *
  * Return: 1 if the character is a whitespace character, 0 otherwise.
  */
-/*int _isspace(char c)*/
 int _isspace(int c)
 {
-	/*if (c == ' ' || c == '\t' || c == '\n' ||*/
-	/*	c == '\v' || c == '\f' || c == '\r')*/
-	/*	return (1);*/
-	/*return (0);*/
-
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v');
+	if (c == ' ' || c == '\t' || c == '\n' ||
+		c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
 }
